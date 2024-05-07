8ec0335b5ff0a71d12883f63e4c3b39c61418e54
manish gautam <manish.gautam@hoogaaa.com>
2024-05-07 13:29:10 +0530

    all set

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const app = express();
 const connect = async () => {
   try {
     await sequelize.authenticate();
-    console.log('All models were synchronized successfully.');
     console.log('Connection has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
@@ -17,9 +16,7 @@ const connect = async () => {
 };
 
 app.use(express.json());
-dotenv.config({
-    path: './.env'
-});
+dotenv.config();
 app.use(cors());
 
 app.use('/v1/api/user', userRouter);
@@ -27,6 +24,6 @@ app.use('/v1/api/user', userRouter);
 const PORT = process.env.PORT || 4040;
 
 app.listen(PORT, async () => {
-  await connect();
+  connect();
   console.log(`Server is running on port ${PORT}`);
 });
